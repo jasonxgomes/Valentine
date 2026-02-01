335add9c2d47857bfb310ca8c27a8e4c246f4336
Jason Gomes <jasonxgomes@icloud.com>
2026-01-31 23:16:47 -0500

    Add files via upload

---
+++ do_you_love_me.js
@@ -0,0 +1,33 @@
+const questionContainer = document.querySelector(".question-container");
+const resultContainer = document.querySelector(".result-container");
+const gifResult = document.querySelector(".gif-result");
+const heartLoader = document.querySelector(".cssload-main");
+const yesBtn = document.querySelector(".js-yes-btn");
+const noBtn = document.querySelector(".js-no-btn");
+
+// change the position of no button
+noBtn.addEventListener("mouseover", () => {
+  const newX = Math.floor(Math.random() * questionContainer.offsetWidth);
+  const newY = Math.floor(Math.random() * questionContainer.offsetWidth);
+
+  noBtn.style.left = `${newX}px`;
+  noBtn.style.top = `${newY}px`;
+});
+
+// yes button functionality
+
+yesBtn.addEventListener("click", () => {
+  questionContainer.style.display = "none";
+  heartLoader.style.display = "inherit";
+
+  setTimeout(() => {
+    document.body.classList.add("fade-out");
+
+    setTimeout(() => {
+      window.location.href = "valentine-confession.html";
+    }, 1200);
+  }, 3000);
+});
+
+
+
